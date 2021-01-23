060f662fd47df6fb7860ed138136d9ed013845e6
MwangiN <65793184+MwangiN@users.noreply.github.com>
2021-01-23 19:30:56 +0300

    Apllication and routes
    mapped paths

--- webapp1/webapp1/urls.py
+++ webapp1/webapp1/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('blog.urls')),#leave it empty since we want blog homepage to be our home page{blog/}
 ]
